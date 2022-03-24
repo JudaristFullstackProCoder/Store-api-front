e7b3674982cdee516a3bf0e15fe065481ca9d751
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-03-25 00:12:31 +0100

    configure React router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import Dashboard from "./components/Dashboard";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import DashboardPage from "./components/pages/Dashboard";
+import ProductsPage from "./components/pages/Products";
+import AnalyticsPage from "./components/pages/Analytics";
+import MessagesPage from "./components/pages/Messages";
+import SettingsPage from "./components/pages/Settings";
+import { AppTitleContext } from "./components/context/AppTitleContext";
 
 function App() {
-  return (
-    <div className="App">
-      
+    return <div className="App">
+    <AppTitleContext.Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard AppTitle="Dashboard" pageElement={<DashboardPage />} />} />
+          <Route path="/Prods" element={<Dashboard AppTitle="Products" pageElement={<ProductsPage />} />} />
+          <Route path="/Analytics" element={<Dashboard AppTitle="Analytics" pageElement={<AnalyticsPage />} />} />
+          <Route path="/Messages" element={<Dashboard AppTitle="Messages" pageElement={<MessagesPage />} />} />
+          <Route path="/Settings" element={<Dashboard AppTitle="Settings" pageElement={<SettingsPage />} />} />
+        </Routes>
+      </BrowserRouter>
+    </AppTitleContext.Provider>
     </div>
-  );
 }
 
 export default App;
