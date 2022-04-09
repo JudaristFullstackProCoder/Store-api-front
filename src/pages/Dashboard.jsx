add6bb30ca380bc15e54dc39acbcc1bec78564aa
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-04-09 13:01:51 +0100

    update Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,4 @@
-import PropTypes from "prop-types";
-import { useContext } from "react";
-import { userContext } from "../context/userContext";
-
 export default function DashboardPage () {
-     const {user} = useContext(userContext);
-     console.log(user);
      return <div>dashboard</div>
 }
 
